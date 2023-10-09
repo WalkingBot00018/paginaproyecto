fc514b0aac0031be0f10c3a0712a08b5435033ee
ruben <rlozanotique@gmail.com>
2023-10-08 22:29:26 -0500

    actualizacion_nuevo_pag

---
+++ pagina_nueva/js/index.js
@@ -0,0 +1,14 @@
+function seleccionar(links){
+    let links =document.querySelectorAll("header nav a");
+
+    links[0].className = "";
+    links[1].className = "";
+    links[2].className = "";
+    links[3].className = "";
+    links[4].className = "";
+    links[5].className = "";
+
+links=className="selected";
+
+
+};
